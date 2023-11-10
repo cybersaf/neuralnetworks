f2fd631f74dd7e01219680b2fe8891b3afcbfac4
Safiyah Sheikh <safiyah@chainhaus.com>
2023-11-10 08:54:07 -0500

    sigmoid error fixed

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -1,18 +1,17 @@
-#neuralnetworks 101
 import random
 import numpy as np
 import math
 
 class Network(object):
-    def __init__(self,sizes):
+    def __init__(self, sizes):
         self.num_layers = len(sizes)
         self.sizes = sizes
-        self.biases = [np.random.randm(y,1) for y in sizes[1:]]
-        self.weights = [np.random.randn(y,x)
-                        for x,y in zip(sizes["-1"],sizes[1:])]
+        self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
+        self.weights = [np.random.randn(y, x) for x, y in zip(sizes[:-1], sizes[1:])]
 
-def feedfwd(self,a):
-    for b,w in zip(self.biases,self.weights):
-        a = sigmoid(np.dot(w,a)+b)
-    return a
+    def feedforward(self, a):
+        for b, w in zip(self.biases, self.weights):
+            a = self.sigmoid(np.dot(w, a) + b)
+        return a
+
                     
